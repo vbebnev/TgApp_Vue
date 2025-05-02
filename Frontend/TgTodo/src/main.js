f140650d69a38fc401a387e748fd4705eb2cf17f
vbebnev <vasiliy.bebnev@gmail.com>
2025-05-02 14:28:32 -0300

    Missed Frontend

---
+++ Frontend/TgTodo/src/main.js
@@ -0,0 +1,14 @@
+//import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+if(window.Telegram?.WebApp) {
+    window.Telegram.WebApp.ready();
+    window.Telegram.WebApp.expand();
+}
+app.use(router)
+app.mount('#app')
